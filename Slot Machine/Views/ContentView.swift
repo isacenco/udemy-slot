a82d0c94e78f93032e937125f80e183379a59076
Ghenadie Isacenco <isacenco@gmail.com>
2025-10-30 16:14:47 +0100

    Main Layout

--- Slot Machine/Views/ContentView.swift
+++ Slot Machine/Views/ContentView.swift
@@ -53,6 +53,50 @@ struct ContentView: View {
                 }
                 
                 // MARK: - SLOT MACHINE
+                VStack(alignment: .center, spacing: 0) {
+                    // MARK: - REEL #1
+                    ZStack {
+                        ReelView()
+                        
+                        Image("gfx-bell")
+                            .resizable()
+                            .modifier(ImageModifier())
+                    }
+                    
+                    HStack(alignment: .center, spacing: 0) {
+                        // MARK: - REEL #2
+                        ZStack {
+                            ReelView()
+                            
+                            Image("gfx-seven")
+                                .resizable()
+                                .modifier(ImageModifier())
+                        }
+                        Spacer()
+                        // MARK: - REEL #3
+                        ZStack {
+                            ReelView()
+                            
+                            Image("gfx-cherry")
+                                .resizable()
+                                .modifier(ImageModifier())
+                        }
+                    }
+                    .frame(maxWidth: 400)
+                    
+                    // MARK: - SPIN BUTTON
+                    Button {
+                        print("Spin The Reels")
+                    } label: {
+                        Image("gfx-spin")
+                            .renderingMode(.original)
+                            .resizable()
+                            .modifier(ImageModifier())
+                    }
+
+                    
+                } // SLOT MACHINE
+                .layoutPriority(2)
                 
                 // MARK: - FOOTER
                 Spacer()
